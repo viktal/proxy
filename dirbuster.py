bf52fc30f7b9be99b0b89146f26cce89f9226009
Viktoria Talmaza <vp.talmaza@gmail.com>
2021-03-15 17:38:15 +0300

    fix dirbuster

--- dirbuster.py
+++ dirbuster.py
@@ -23,7 +23,10 @@ def read_file():
 
 def check_response_code(path, site):
     url = f"{site}/{path}"
-    response = requests.get(url, allow_redirects=False)
+    try:
+        response = requests.get(url, allow_redirects=False)
+    except ConnectionError:
+        return
     if response.status_code != 404:
         # print(f'Code: {response.status_code} Url: {url}')
         return {'code': response.status_code, 'url': url}
@@ -32,10 +35,7 @@ def check_response_code(path, site):
 
 def dibuster(site):
     site = parse_url(site)
-    response = requests.get(site)
-    # if response.status_code != 200:
-    #     return f'Url {site} get status code {response.status_code}'
-
+    print('Dirbuster start work')
     path = read_file()
     with Pool(processes=config.COUNT_CPU*config.COUNT_CPU) as pool:
         result = pool.starmap(check_response_code, [(e, site) for e in path])
